01f234af64cc41fab21cd8da28c8930f71204aca
Chirag Sharma <125489712+ChiragIITB@users.noreply.github.com>
2023-12-11 17:32:24 +0530

    Update script.js
    Added javascript for basic animations and hamburger

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const animate = document.querySelectorAll('.content-box');
 
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//  Hamburger js
+
+const menuBtn = document.querySelector('.hamburger');
+const menuContent = document.querySelector('.mobile-nav');
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('is-active');
+    menuContent.classList.toggle('is-active');
+});
