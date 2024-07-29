e59f1e0b15845f84cace05faaad0f90529768929
Chirag Sharma <125489712+ChiragIITB@users.noreply.github.com>
2024-07-29 16:33:31 +0530

    Update script.js
    Updating the script - 29July2024

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 
 // .............loader script
 
-addEventListener('DOMContentLoaded',() => {
+// addEventListener('DOMContentLoaded',() => {
 
-    setTimeout(() => {
-        hideLoader();
-        showBody();
-    }, 2000)
+//     setTimeout(() => {
+//         hideLoader();
+//         showBody();
+//     }, 2000)
 
-    function hideLoader(){
-        const loader = document.querySelector('.loader');
-        loader.style.display = "none";
-    }
-    function showBody(){
-        const bodyContent = document.querySelector('.body-content');
-        bodyContent.style.display = "block";    }
-})
-
+//     function hideLoader(){
+//         const loader = document.querySelector('.loader');
+//         loader.style.display = "none";
+//     }
+//     function showBody(){
+//         document.body.style.display = "block";    }
+// })
 
 
 
@@ -55,28 +53,101 @@ menuBtn.addEventListener('click', function(){
 
 
 
+// ...........Events Animation Js
 
-// ...................Blurring on scroll
+const eventImage = document.querySelectorAll('.event-image');
+const eventText = document.querySelectorAll('.event-text');
+
+eventImage.forEach((el) => {
+    el.addEventListener('click', (e) =>{
+        console.log(e);
+        el.style.opacity = "0";
+        console.log(el);
+        const targText = el.nextElementSibling;  
+        console.log(targText);
+        targText.style.opacity = "1";
+        targText.style.zIndex  = "100";
+    });
+});
+eventText.forEach((el) => {
+    el.addEventListener('click', (e) =>{
+        console.log(e);
+        console.log(el);
+        el.style.opacity = "0";
+        el.style.zIndex  = "-1";
+        const targImage = el.previousElementSibling; 
+        targImage.style.opacity = "1";
+    });
+});
 
 
 
-const home = document.querySelector('#home');
-
-const pageHeight = home.clientHeight;
-
-console.log(pageHeight);
-
-window.addEventListener('scroll', () => {
-    let ratio = window.scrollY / pageHeight ;
-
-    if(ratio<=2){
-        document.body.style.backgroundImage = `linear-gradient(to right, rgba(80, 124, 158, ${1.1*ratio}), rgba(80, 124, 158, ${1.1*ratio})), url(../images/main-background.png)` ;
-    }
-
-    else if(ratio>2){
-        document.body.style.backgroundImage = `url(images/main-background.png)`;
-    }
-})
+// ......Implementations Scroll JS
 
 
+// const rightScroll = document.querySelector('.rightScroll');
+// const leftScroll = document.querySelector('.leftScroll');
 
+// const slides = document.querySelectorAll('.impBox');
+
+// rightScroll.addEventListener('click', () => {
+//     let count = 0;
+//     slides.forEach((slide) => {
+//         if(isInViewport(slide) && count === 0){
+//             console.log(slide.nextElementSibling);
+//             slide.nextElementSibling.scrollIntoView({behavior: "smooth", inline:"center", block:"center"});
+//             count += 1;
+//             console.log(`Count : ${count}`);
+//         }
+//     })
+// })
+// leftScroll.addEventListener('click', () => {
+//     let count = 0;
+//         slides.forEach((slide) => {
+//         if(isInViewport(slide) && count === 0){
+//             console.log(slide.previousElementSibling);
+//             slide.previousElementSibling.scrollIntoView({behavior: "smooth", inline:"center", block: 'center'});
+//             count += 1;
+//             console.log(`Count : ${count}`);
+//         }
+//     })
+// })
+
+// console.log(window.innerWidth)
+// console.log(document.documentElement.clientWidth)
+
+// function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.left >= 0 &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
+
+
+
+// ........Scale on Scroll Animations
+
+
+// const thresholds = [];
+
+// for(let i=0; i<=1; i+=0.01)
+// {
+//     thresholds.push(i);
+// }
+
+// const ScaleElements = function ScaleElement(entries){
+//     entries.forEach((element) => {
+//         if(element.isIntersecting){
+//             element.target.style.transform = `translateX(${350*(1-element.intersectionRatio)*(1-element.intersectionRatio)*(1-element.intersectionRatio)}px)`;
+//         }
+//     });
+// };
+
+// const ScaleObserver = new IntersectionObserver(ScaleElements, {threshold: thresholds})
+
+// const AllElements = document.querySelectorAll('.right-flow-animate');
+
+// AllElements.forEach((element) => {
+//     ScaleObserver.observe(element);
+// })
